9cfeddfc39bf48d2bcb3e1c140a16bd5f4aefb9a
Jackson Donovan <iinventedtwitter@gmail.com>
2019-01-25 14:36:52 -0700

    started styling on splash and about pages

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -1,22 +1,33 @@
 import React from 'react';
+import { Grid, Row, Col, Image } from 'react-bootstrap';
+import '../stylesheets/about-style.css';
 
 const About = () => {
   return (
     <div>
-      <h5>Our team is dedicated to making sure everyone has access
-      to understanding the basics of investing!</h5>
-      <h6>No matter your age or income, it's never too late to start.
+      <h2 className="header-footer-titles">Investing simplified!</h2>
+      <h4 className="header-footer-titles">No matter your age or income, it's never too late to start.</h4>
+      <h4 className="header-footer-titles">
       We'll help you better understand all types of investments,
       from risky to secure, from property to stocks, anything is possible
-      if you start early enough.</h6>
+      if you start early enough.</h4>
 
       <hr />
-
-      <h4>Meet the team!</h4>
-      <h4>Jackson Donovan</h4>
-      <h5>Lead Developer, Finance Enthusiast</h5>
-      <h4>Bryan Bathalon</h4>
-      <h5>Financial Expert</h5>
+      <h2 className="header-footer-titles">Meet the Team</h2>
+      <Grid>
+        <Row>
+          <Col xs={6} md={4}>
+            <h4 className="header-footer-titles">Jackson Donovan</h4>
+            <Image className="pic-circle" src="https://scontent.fapa1-2.fna.fbcdn.net/v/t31.0-8/18216857_10212856826920493_1606629318825930360_o.jpg?_nc_cat=109&_nc_ht=scontent.fapa1-2.fna&oh=5be0f45f375dda0ea0e85c2ec161ecac&oe=5CF95C8D" circle />
+            <h5 className="header-footer-titles">Lead Developer, Finance Enthusiast</h5>
+          </Col>
+          <Col xs={6} md={4}>
+            <h4 className="header-footer-titles">Bryan Bathalon</h4>
+            <Image className="pic-circle" src="https://scontent.fapa1-2.fna.fbcdn.net/v/t1.0-9/10365803_10202630735624531_2093832118338443939_n.jpg?_nc_cat=105&_nc_ht=scontent.fapa1-2.fna&oh=a86a07b89d2f7ac408ce4420ffb8ac85&oe=5CB97E4E" circle />
+            <h5 className="header-footer-titles">Financial Expert</h5>
+          </Col>
+        </Row>
+      </Grid>
     </div>
   );
 }
--- client/src/components/Splash.js
+++ client/src/components/Splash.js
@@ -1,14 +1,70 @@
 import React from 'react';
+import { Jumbotron, Button, Grid, Row, Col, Thumbnail } from 'react-bootstrap';
+import '../stylesheets/splash-style.css';
 
 const Splash = () => {
   return (
     <div>
-      <h3>Welcome to I Should Invest!</h3>
-      <h5>Too many times we hear that Millennials and
-      young folks want to invest, but don't understand
-      the logistics or where to start.</h5>
-      <hr />
-      <h5>Our team is dedicated to change that!</h5>
+      <Jumbotron className="tall-jumbo">
+        <h2 className="header-footer-titles">Welcome to I Should Invest!</h2>
+        <p className="header-footer-titles">
+          Too many times we hear that Millennials and
+          young folks want to invest, but don't understand
+          the logistics or where to start.
+        </p>
+        <hr />
+        <p className="header-footer-titles">
+          Our team is changing that.
+        </p>
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <p className="header-footer-titles">
+          <Button bsStyle="primary">Get Started</Button>
+        </p>
+      </Jumbotron>
+
+      <Grid>
+        <Row>
+          <Col xs={6} md={4}>
+            <Thumbnail src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTFhyregOtAoVfiJgGVW8eIct6IKgkiK6pXGu94Z_UbjzAH-lhi" alt="242x200">
+              <h3 className="header-footer-titles">Stocks</h3>
+              <p>Understand how stocks work and how to move beyond judging a stock by its price. Become an invester not a trader.</p>
+              <p>
+                <Button bsStyle="primary">Learn More</Button>
+              </p>
+            </Thumbnail>
+          </Col>
+          <Col xs={6} md={4}>
+            <Thumbnail src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT9uNbhqG6fWzXc3wNc3kPqfbKoJTVfPfDOwe6Qn2sIWy6lMOUu" alt="242x200">
+              <h3 className="header-footer-titles">Property</h3>
+              <p>Housing can be expensive but isn't always a strong investment. Find out what makes housing risky and rewarding.</p>
+              <p>
+                <Button bsStyle="primary">Learn More</Button>
+              </p>
+            </Thumbnail>
+          </Col>
+          <Col xs={6} md={4}>
+            <Thumbnail src="https://thecorrespondent.in/wp-content/uploads/2018/09/Vacation.jpg" alt="242x200">
+              <h3 className="header-footer-titles">Retirement Accounts</h3>
+              <p>Retiring can be tricky, how much do you need? What accounts are worth opening? Let's put the me back in retirement.</p>
+              <p>
+                <Button bsStyle="primary">Button</Button>
+                &nbsp;
+                <Button bsStyle="default">Button</Button>
+              </p>
+            </Thumbnail>
+          </Col>
+        </Row>
+      </Grid>
+
+
     </div>
   );
 }
